072f1b818b1b00fcc2148e938258298fb50202eb
youssefhoussam <129467036+youssefhoussam@users.noreply.github.com>
2025-10-05 15:14:07 +0100

    Build site

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   base: "/yohoussam-portfolio-chronicle/",
   server: {
@@ -15,5 +14,8 @@ export default defineConfig(({ mode }) => ({
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
+  },
+  build: {
+    outDir: "docs", 
   },
 }));
