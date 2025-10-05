3389c5abf0ac8028b8a324ade5d8383e3ca3d369
youssefhoussam <129467036+youssefhoussam@users.noreply.github.com>
2025-10-05 15:37:02 +0100

    Deploy site from root

--- vite.config.ts
+++ vite.config.ts
@@ -2,33 +2,20 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
-import { viteStaticCopy } from "vite-plugin-static-copy";
 
 export default defineConfig(({ mode }) => ({
-  base: "/yohoussam-portfolio-chronicle/",
+  base: "/yohoussam-portfolio-chronicle/", 
   server: {
     host: "::",
     port: 8080,
   },
-  plugins: [
-    react(),
-    mode === "development" && componentTagger(),
-    viteStaticCopy({
-      targets: [
-        {
-          src: "docs/index.html",
-          dest: ".",
-          rename: "404.html",
-        },
-      ],
-    }),
-  ].filter(Boolean),
+  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
   build: {
-    outDir: "docs",
+    outDir: ".", 
   },
 }));
